f454d14ff76a883cc9e9a21cd628dbdc18a1f00e
Robert Pace <rpace1994@gmail.com>
2022-06-25 03:51:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Welcome This is a game name generator 
+#1.  Create a greeting for your program.
+
+print("Welcome to the game name generator.")
+
+#2. Ask the user for the name of a pet
+
+city = input("Which city did you grow up in?\n")
+
+#3. Ask the user for the name of a pet.
+
+pet = input("What is the name of your pet?\n")
+
+#4. combine the name of heir city and pet and show them their game name
+
+print("Your band name could be " + city + " " + pet)
+
+#5. Make sure the input cusor shows on a new line 
